3824b2804bb41f5f6f5d60e793342f7eca6cb5cd
vitya <levchenko.victor@gmail.com>
2018-07-25 21:39:01 +0300

    Raks names

--- src/Exercises/Rank.java
+++ src/Exercises/Rank.java
@@ -10,5 +10,20 @@ package Exercises;
  * @author lva
  */
 public enum Rank {
+    DEUCE   ("2"), 
+    THREE   ("3"), 
+    FOUR    ("4"), 
+    FIVE    ("5"), 
+    SIX     ("6"), 
+    SEVEN   ("7"),
+    EIGHT   ("8"), 
+    NINE    ("9"), 
+    TEN     ("10"), 
+    JACK    ("Jack"), 
+    QUEEN   ("Queen"), 
+    KING    ("King"), 
+    ACE     ("Ace");
     
+    private String name;
+    Rank(String name) { this.name = name; }    
 }
