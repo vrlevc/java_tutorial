36045dc1164058ef199a5efc85b94d9e2c1c89b0
glvlevch <viktor.levchenko@avid.com>
2018-11-19 18:14:03 +0200

    Console - does not work for me

--- test/javatutorial/BasicIO.java
+++ test/javatutorial/BasicIO.java
@@ -17,8 +17,10 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Console;
 import java.io.PrintWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -146,5 +148,51 @@ public class BasicIO {
         System.out.format("Teh square root of %d is %f.%n", 2, Math.sqrt(2));
         System.out.format("%f, %1$+020.10f %n", Math.PI);
         System.out.format("%f, %<+020.10f %n", Math.PI);
+    }
+    
+    @Test
+    public void testConsole() {
+        Console c = System.console();
+        if (c == null) {
+            System.out.println("No console.");
+        }
+        assertNotNull(c);
+        
+        String login = c.readLine("Enter your login: ");
+        char [] oldPassword = c.readPassword("Enter your old password: ");
+
+        if (verify(login, oldPassword)) {
+            boolean noMatch;
+            do {
+                char [] newPassword1 = c.readPassword("Enter your new password: ");
+                char [] newPassword2 = c.readPassword("Enter new password again: ");
+                noMatch = ! Arrays.equals(newPassword1, newPassword2);
+                if (noMatch) {
+                    c.format("Passwords don't match. Try again.%n");
+                } else {
+                    change(login, newPassword1);
+                    c.format("Password for %s changed.%n", login);
+                }
+                Arrays.fill(newPassword1, ' ');
+                Arrays.fill(newPassword2, ' ');
+            } while (noMatch);
+        }
+
+        Arrays.fill(oldPassword, ' ');
+    }
+    
+    // Dummy change method.
+    static boolean verify(String login, char[] password) {
+        // This method always returns
+        // true in this example.
+        // Modify this method to verify
+        // password according to your rules.
+        return true;
+    }
+
+    // Dummy change method.
+    static void change(String login, char[] password) {
+        // Modify this method to change
+        // password according to your rules.
     }
 }
