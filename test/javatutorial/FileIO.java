4d455ea9b052315970f0bcc35f1e5779cdd7df10
glvlevch <viktor.levchenko@avid.com>
2018-11-20 10:37:17 +0200

    File IO - new trials

---
+++ test/javatutorial/FileIO.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javatutorial;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author vitya
+ */
+public class FileIO {
+
+    
+    
+}
