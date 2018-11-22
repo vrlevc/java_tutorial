7114544671b8639b1b2a033916b2717e1cdf7179
Vitya <levchenko.victor@gmail.com>
2018-11-22 09:04:01 +0200

    Creating a Path.

--- test/javatutorial/FileIO.java
+++ test/javatutorial/FileIO.java
@@ -5,6 +5,10 @@
  */
 package javatutorial;
 
+import java.net.URI;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -14,6 +18,19 @@ import static org.junit.Assert.*;
  */
 public class FileIO {
    
-    
-    
+   @Test
+   public void testCreatePath() {
+       Path p1 = Paths.get("/tmp/foo");
+       Path p2 = Paths.get(URI.create("file:///Users/joe/FileTest.java"));
+       
+       // Paths.get is a:
+       Path p4 = FileSystems.getDefault().getPath("/users/sally");
+       
+       // Creating /u/joe/logs/foo.log assuming your home directory is /u/joe, 
+       // or C:\joe\logs\foo.log if you are on Windows.
+       Path p5 = Paths.get(System.getProperty("user.home"), "logs", "foo.log");
+   }
+   
+   
+   
 }
