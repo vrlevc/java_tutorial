62267d59e5cbdf9341849af91b353d06abef95af
Vitya <levchenko.victor@gmail.com>
2018-11-22 13:19:37 +0200

    Retrieving Information about a Path.

--- test/javatutorial/FileIO.java
+++ test/javatutorial/FileIO.java
@@ -31,6 +31,34 @@ public class FileIO {
        Path p5 = Paths.get(System.getProperty("user.home"), "logs", "foo.log");
    }
    
+   public static void printPathInfo(Path path) {
+       System.out.format("toString: %s%n", path.toString());
+       System.out.format("getFileName: %s%n", path.getFileName());
+       System.out.format("getName(0): %s%n", path.getName(0));
+       System.out.format("getNameCount: %s%n", path.getNameCount());
+       System.out.format("subpath(0, 2): %s%n", path.subpath(0, 2));
+       System.out.format("getParent: %s%n", path.getParent());
+       System.out.format("getRoot: %s%n", path.getRoot());
+   }
    
+   @Test
+   public void testPathInfo() {
+       
+       // None of these methods requires that the file corresponding
+       // to the Path exists.
+       
+       // Microsoft Windows
+       Path pathPC = Paths.get("C:\\home\\joe\\foo");
+       
+       // Solaris 
+       Path pathSL = Paths.get("/home/joe/foo");
+       
+       System.out.println("Microsoft Windows : \"c:\\home\\joe\\foo\"");
+//       printPathInfo(pathPC);
+       
+       System.out.println("Solaris : \"/home/joe/foo\"");
+//       printPathInfo(pathSL);
+       
+   }
    
 }
